54ac852693feeb1d0607778a9ac99defcf9f4cd8
Seppe Lenaerts <seppe.lenaerts@ae.be>
2025-08-15 16:37:40 +0200

    Pick apples from tree

--- scenes/objects/nature/mouse_area.gd
+++ scenes/objects/nature/mouse_area.gd
@@ -20,4 +20,4 @@ func _unhandled_input(event):
 	if mouse_in_area and event is InputEventMouseButton:
 		var mouse_event := event as InputEventMouseButton
 		if mouse_event.button_mask == MOUSE_BUTTON_MASK_LEFT:
-			print("WOIOIOIOIWIIW")
+			sprite.hide()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,6 +6,6 @@ extends CharacterBody2D
 const SPEED = 233.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
 	move_and_slide()
