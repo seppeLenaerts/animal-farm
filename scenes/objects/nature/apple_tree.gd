f14529f8d61fa3a8f1c7e1747a3e761674599eac
Seppe <lenaertssep@gmail.com>
2025-08-15 21:06:18 +0200

    Plug in the player near object detector

--- scenes/objects/nature/apple_tree.gd
+++ scenes/objects/nature/apple_tree.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 @export var mouse_area : MouseDetector
+@export var player_near_object : IsPlayerNearObjectDetector
 @export var sprite_to_hide : Sprite2D
 
 func _ready() -> void:
@@ -8,11 +9,13 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if (mouse_area.mouse_in_area):
+	if (mouse_area.mouse_in_area and player_near_object.is_close):
 		sprite_to_hide.modulate = Color.GRAY
 	else:
 		sprite_to_hide.modulate = Color.WHITE
 
 
 func _apple_tree_clicked() -> void:
-	sprite_to_hide.hide()
+	if (player_near_object.is_close):
+		sprite_to_hide.hide()
+		GameManagerGlobal.add_apple()
--- scenes/objects/nature/tree_player_area.gd
+++ scenes/objects/nature/tree_player_area.gd
@@ -6,13 +6,11 @@ var is_close = false
 
 func _on_body_entered(body: Node2D):
 	if body is Player:
-		print("Ik ben bij een boom")
 		is_close = true
 
 func _on_body_exited(body: Node2D):
 	if body is Player:
 		is_close = false
-		print("Ik ben NIET bij een boom")
 
 func _ready():
 	body_entered.connect(_on_body_entered)
