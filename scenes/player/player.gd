5b3fcf324bfd3d95caeefac164f7718236aed2a2
NikitaTkachenko <nikita.tkachenko@ae.be>
2025-08-15 19:09:53 +0200

    duck can now be zoomed

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -10,3 +10,13 @@ const SPEED = 233.0
 func _physics_process(_delta: float) -> void:
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
 	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		var mouse_event := event as InputEventMouseButton
+		var zoom_vector := camera.zoom
+		if mouse_event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom_vector -= Vector2(0.1, 0.1)
+		elif mouse_event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom_vector += Vector2(0.1, 0.1)
+		camera.zoom = zoom_vector.clamp(Vector2(1.0, 1.0), Vector2(4.5, 4.5))
