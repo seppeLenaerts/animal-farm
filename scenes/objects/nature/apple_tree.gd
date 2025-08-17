b6e72911f9100ee66a5f9c1a6704ef7fdb3fa332
Seppe <lenaertssep@gmail.com>
2025-08-17 22:47:54 +0200

    Add animated trees and ground

--- scenes/objects/nature/apple_tree.gd
+++ scenes/objects/nature/apple_tree.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 @export var mouse_area : MouseDetector
 @export var player_near_object : PlayerDetector
-@export var sprite_to_hide : Sprite2D
+@export var sprite : AnimatedSprite2D
 
 func _ready() -> void:
 	mouse_area.area_clicked.connect(_apple_tree_clicked)
@@ -10,12 +10,12 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if (mouse_area.mouse_in_area and player_near_object.is_close):
-		sprite_to_hide.modulate = Color.GRAY
+		sprite.modulate = Color.GRAY
 	else:
-		sprite_to_hide.modulate = Color.WHITE
+		sprite.modulate = Color.WHITE
 
 
 func _apple_tree_clicked() -> void:
 	if (player_near_object.is_close):
-		sprite_to_hide.hide()
+		sprite.play()
 		GameManagerGlobal.add_apple()
