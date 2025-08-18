6e36a9e29e7cda21661a5cc13ac00f026462059e
Seppe <lenaertssep@gmail.com>
2025-08-18 19:07:32 +0200

    Add carrots?!

--- game_manager.gd
+++ game_manager.gd
@@ -3,6 +3,8 @@ extends Node2D
 var apples = 0
 var stamina = 100.0
 
+signal day_passes
+
 func _ready() -> void:
 	pass
 
@@ -14,5 +16,10 @@ func _process(_delta: float) -> void:
 func add_apple() -> void:
 	apples += 1
 
+
 func use_stamina(consumed: float) -> void:
 	stamina -= consumed
+
+
+func pass_day() -> void:
+	day_passes.emit()
---
+++ scenes/objects/nature/carrot.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var mouse_detector : MouseDetector
+@export var player_detector : PlayerDetector
+@export var sprite : AnimatedSprite2D
+
+var harvestable = false
+var total_frames : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	mouse_detector.area_clicked.connect(_on_area_clicked)
+	GameManagerGlobal.day_passes.connect(_continue_stage)
+	total_frames = sprite.sprite_frames.get_frame_count("default")
+
+
+func _process(_delta):
+	if (player_detector.is_close and mouse_detector.mouse_in_area):
+		sprite.modulate = Color.GRAY
+	else:
+		sprite.modulate = Color.WHITE
+
+
+func _on_area_clicked() -> void:
+	if (player_detector.is_close and harvestable):
+		GameManagerGlobal.use_stamina(5)
+		queue_free()
+
+
+func _set_harvestable() -> void:
+	harvestable = true
+
+
+func _continue_stage() -> void:
+	if (sprite.frame < total_frames):
+		sprite.frame += 1
+
+	if (sprite.frame == total_frames - 1):
+		_set_harvestable()
