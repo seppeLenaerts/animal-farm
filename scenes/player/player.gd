3ead479ce7279e5771ebc7820bd4175b11ab6eff
Nikita Tkachenko <nikita.tkachenko01@gmail.com>
2025-08-22 00:30:54 +0200

    added swim on water via custom data layer. Need improvement on tile coordinates.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,28 +4,51 @@ class_name Player
 
 @export var camera : Camera2D
 @export var sprite : AnimatedSprite2D
+@export var tilemaplayer : TileMapLayer
 
 const SPEED = 133.0
 
+const ANIMATION_MAPPING := {
+	"water": {
+		"sideways": "sideways_water",
+		"up": "up_water",
+		"down": "down_water",
+		},
+	"ground": {
+		"sideways": "sideways",
+		"up": "up",
+		"down": "down",
+		}
+}
+
 
 func _physics_process(_delta: float) -> void:
+	var cell := tilemaplayer.get_cell_tile_data(_get_tile_index())
+	var is_water_tile : bool = cell.get_custom_data("is_water_tile")
+	print(is_water_tile)
 	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * SPEED
-	
-	_select_animation()
+	_select_animation(is_water_tile)
 	_determine_flip_sprite()
 	_play_animation_if_moving()
 	_consume_stamina_if_moving()
 	move_and_slide()
 	
 
-func _select_animation():
-	if velocity.y > 0 and velocity.x == 0:
-		sprite.animation = "down"
-	elif velocity.y < 0 and velocity.x == 0:
-		sprite.animation = "up"
-	elif velocity.x != 0:
-		sprite.animation = "sideways"
-
+func _select_animation(is_water_tile: bool = false):
+	if is_water_tile:
+		if velocity.y > 0 and velocity.x == 0:
+			sprite.animation = "down_water"
+		elif velocity.y < 0 and velocity.x == 0:
+			sprite.animation = "up_water"
+		elif velocity.x != 0:
+			sprite.animation = "sideways_water"
+	else:
+		if velocity.y > 0 and velocity.x == 0:
+			sprite.animation = "down"
+		elif velocity.y < 0 and velocity.x == 0:
+			sprite.animation = "up"
+		elif velocity.x != 0:
+			sprite.animation = "sideways"
 
 func _determine_flip_sprite():
 	if velocity.x > 0:
@@ -33,13 +56,11 @@ func _determine_flip_sprite():
 	elif velocity.x < 0:
 		sprite.flip_h = false
 
-
 func _play_animation_if_moving():
 	if velocity != Vector2.ZERO:
 		sprite.play()
 	else:
 		sprite.stop()
-
 
 func _consume_stamina_if_moving():
 	if velocity != Vector2.ZERO:
@@ -54,3 +75,6 @@ func _unhandled_input(event):
 		elif mouse_event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			zoom_vector += Vector2(0.1, 0.1)
 		camera.zoom = zoom_vector.clamp(Vector2(1.0, 1.0), Vector2(4.5, 4.5))
+
+func _get_tile_index():
+	return Vector2i(int(self.position[0] / 15), int(self.position[1] / 15))
